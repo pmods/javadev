3fd17133eec06b2d926fae704e402b33a4061788
Andrew Marks <andrew.marks@navy.mil>
2014-07-10 16:28:58 -0700

    Manually install ant into /opt on centos

---
+++ files/ant.sh
@@ -0,0 +1,4 @@
+ANT_HOME=/opt/apache-ant-1.9.4
+PATH=${PATH}:${ANT_HOME}/bin
+
+export PATH ANT_HOME
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,11 @@
 class javadev {
 
+    $defpath = "/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin"
+    $modpath = 'puppet:///modules/javadev'
+
     case $::operatingsystem {
         FreeBSD: {
+            $manualant = false
 
             include pkgng
 
@@ -19,14 +23,16 @@ class javadev {
                 'java-1.6.0-openjdk-devel',
                 'java-1.7.0-openjdk',
                 'java-1.7.0-openjdk-devel',
-                'ant',
-                'ant-apache-regexp',
             ]
+
+            $manualant = true
 
             $pkg_provider = yum
         }
         default: {
             
+            $manualant = false
+
             $packages = [
                 'openjdk',
                 'openjdk6',
@@ -41,6 +47,44 @@ class javadev {
     package { $packages:
         ensure   => installed,
         provider => $pkg_provider
+    }
+
+    if $::operatingsystem == 'centos' {
+        package { ['ant', 'ant-apache-regexp']:
+            ensure   => purged,
+        }
+    }
+
+    # Install ant manually
+    if $manualant  {
+        $ant_url = "http://mirror.cogentco.com/pub/apache//ant/binaries"
+        $ant_version = "apache-ant-1.9.4-bin"
+
+        exec { 'fetch-ant':
+            command => "curl -o $ant_version.tar.gz $ant_url/$ant_version.tar.gz",
+            user    => 'root',
+            cwd     => '/root',
+            unless  => "ls /root/$ant_version.tar.gz",
+            path    => $defpath
+        }
+
+        exec { 'extract-ant':
+            command => "tar xzf /root/$ant_version.tar.gz",
+            user    => 'root',
+            cwd     => '/opt',
+            unless  => "ls -d /opt/$ant_version",
+            path    => $defpath,
+            require => Exec['fetch-ant']
+        }
+
+        file { 'ant-env':
+            path => '/etc/profile.d/ant.sh',
+            ensure => file,
+            mode => 0644,
+            owner => 'root',
+            group => 'root',
+            source => "$modpath/ant.sh"
+        }
     }
 
     #TODO: install latest ivy
